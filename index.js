9f04781d40426cba7dcea1d583c77b0cdceec19a
Stefan Pla <spla@icloud.com>
2024-01-08 15:50:10 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+
+function appendCat(name){
+    const moreCats = cats.slice();
+    moreCats.push(name);
+    return moreCats;
+}
+
+function prependCat(name){
+    const fewerCats = cats.slice();
+    fewerCats.unshift(name);
+    return fewerCats;
+}
+
+function removeLastCat(){
+    const oneLessCat = cats.slice(0, cats.length-1);
+    return oneLessCat;
+}
+
+function removeFirstCat(){
+    const fewerCats = cats.slice(1);
+    return fewerCats;
+}
